46ac1a155090e95eb906d2419d66a2bdfa17cda6
HyunMin Lee <hmlee4135@naver.com>
2022-07-13 02:11:42 +0900

    캐릭터 선택과 저장

--- TerZilLangMalLang_JJin/Assets/2. HM/2. Scripts/Char_Choice/Character_Choice_Manager.cs
+++ TerZilLangMalLang_JJin/Assets/2. HM/2. Scripts/Char_Choice/Character_Choice_Manager.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Character
 {
     public GameObject char_Img;
     public string name;
+    public string concept_Txt;
     public int nft_Number;
 
-    public Character(GameObject _char_Img, string _name, int _nft_Number)
+
+
+    public Character(GameObject _char_Img, string _name, int _nft_Number, string _concept)
     {
         char_Img = _char_Img;
         name = _name;
         nft_Number = _nft_Number;
+        concept_Txt = _concept;
     }
 }
 
@@ -20,10 +26,13 @@ public class Character_Choice_Manager : MonoBehaviour
 {
     public GameObject[] character_Img; // 캐릭터 이미지 배열
     int selectCharCount = 0;           // 캐릭터 선택 번호
+    int getKeyNum;
 
     Dictionary<int, Character> characterChoice;
 
-
+    public TextMeshProUGUI debug_Name;
+    public TextMeshProUGUI debug_Nft_Number;
+    public TextMeshProUGUI debug_concept;
 
     // Start is called before the first frame update
     void Start()
@@ -34,35 +43,26 @@ public class Character_Choice_Manager : MonoBehaviour
 
         string name;
 
-
         name = "원더 드래곤";
-        characterChoice.Add(0, new Character(character_Img[0], name, 1234523));
+        characterChoice.Add(0, new Character(character_Img[0], name, 1234523,"노랭이"));
 
         name = "엔젤 드래곤";
-        characterChoice.Add(1, new Character(character_Img[1], name, 323451));
+        characterChoice.Add(1, new Character(character_Img[1], name, 323451,"검흰댕이"));
 
         name = "위너 드래곤";
-        characterChoice.Add(2, new Character(character_Img[2], name, 544353));
+        characterChoice.Add(2, new Character(character_Img[2], name, 544353,"검댕이무지개"));
 
         name = "베리 드래곤";
-        characterChoice.Add(3, new Character(character_Img[3], name, 54634));
+        characterChoice.Add(3, new Character(character_Img[3], name, 54634,"핑크핑쿠"));
 
         name = "지니 드래곤";
-        characterChoice.Add(4, new Character(character_Img[4], name, 987654));
+        characterChoice.Add(4, new Character(character_Img[4], name, 987654,"퍼랭이"));
 
        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     public void LeftBtn_Clik()
     {
-       
-
         selectCharCount--;                              
         if(selectCharCount < 0)
         {
@@ -80,6 +80,7 @@ public class Character_Choice_Manager : MonoBehaviour
         //character_Img[selectCharCount].SetActive(true);
         Character charf1 = characterChoice[selectCharCount];
         charf1.char_Img.SetActive(true);
+        
     }
 
     public void RightBtn_Clik()
@@ -92,5 +93,16 @@ public class Character_Choice_Manager : MonoBehaviour
             character_Img[i].SetActive(false);
         }
         character_Img[selectCharCount].SetActive(true);
+    }
+
+    public void ChoiceBtn_Clik()
+    {
+        if(characterChoice.ContainsKey(selectCharCount))
+        {
+            Character choiceNow = characterChoice[selectCharCount];
+            debug_Name.text = $"디버그 이름 : {choiceNow.name}";
+            debug_Nft_Number.text = $"디버그 NFT 번호 : {choiceNow.nft_Number}";
+            debug_concept.text = $"디버그 배경설명 : {choiceNow.concept_Txt}";
+        }
     }
 }
