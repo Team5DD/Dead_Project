c111ff2a1bcd4da5d67780995da65fd4db3c491d
HyunMin Lee <hmlee4135@naver.com>
2022-07-14 03:21:59 +0900

    캐릭터 선택 UI 완료

--- TerZilLangMalLang_JJin/Assets/2. HM/2. Scripts/UI_Test/Char_Choice/Character_Choice_Manager.cs
+++ TerZilLangMalLang_JJin/Assets/2. HM/2. Scripts/UI_Test/Char_Choice/Character_Choice_Manager.cs
@@ -9,13 +9,15 @@ using UnityEngine.SceneManagement;
 public class Character
 {
     public GameObject char_Img;
+    public GameObject char_Info;
     public string name;
     public string concept_Txt;
     public int nft_Number;
 
-    public Character(GameObject _char_Img, string _name, int _nft_Number, string _concept)
+    public Character(GameObject _char_Img, GameObject _char_Info, string _name, int _nft_Number, string _concept)
     {
         char_Img = _char_Img;
+        char_Info = _char_Info;
         name = _name;
         nft_Number = _nft_Number;
         concept_Txt = _concept;
@@ -25,15 +27,11 @@ public class Character
 public class Character_Choice_Manager : MonoBehaviour
 {
     public GameObject[] character_Img; // 캐릭터 이미지 배열
-    int selectCharCount = 0;           // 캐릭터 선택 번호
+    public GameObject[] character_Info; 
+    public int selectCharCount = 0;           // 캐릭터 선택 번호
     int getKeyNum;
 
     public Dictionary<int, Character> characterChoice;
-
-    public TextMeshProUGUI debug_Name;
-    public TextMeshProUGUI debug_Nft_Number;
-    public TextMeshProUGUI debug_concept;
-    public TextMeshProUGUI debug_stageClear;
 
     public Button gameStart;
 
@@ -42,69 +40,109 @@ public class Character_Choice_Manager : MonoBehaviour
     public Image dragon4_image;
     public Image dragon5_image;
     public Image[] crown_Image;
-    //public Image crown2_Image;
-    //public Image crown3_Image;
-    //public Image crown4_Image;
                              
     public Sprite[] drangons;
     public Sprite[] crowns;
 
-    int debug_num = 0;
-
+    public int debug_StageClear = 0;
     // Start is called before the first frame update
     void Start()
     {
         character_Img[selectCharCount].SetActive(true); // 0번 캐릭터 이미지 활성화
-
+        character_Info[selectCharCount].SetActive(true); 
         characterChoice = new Dictionary<int, Character>();
 
         string name;
 
         name = "원더 드래곤";
-        characterChoice.Add(0, new Character(character_Img[0], name, 1234523, "노랭이"));
+        characterChoice.Add(0, new Character(character_Img[0],character_Info[0], name, 1234523, "노랭이"));
         name = "엔젤 드래곤";
-        characterChoice.Add(1, new Character(character_Img[1], name, 323451, "검흰댕이"));
+        characterChoice.Add(1, new Character(character_Img[1],character_Info[1], name, 323451, "검흰댕이"));
         name = "위너 드래곤";
-        characterChoice.Add(2, new Character(character_Img[2], name, 544353, "검댕이무지개"));
+        characterChoice.Add(2, new Character(character_Img[2],character_Info[2], name, 544353, "검댕이무지개"));
         name = "베리 드래곤";
-        characterChoice.Add(3, new Character(character_Img[3], name, 54634, "핑크핑쿠"));
+        characterChoice.Add(3, new Character(character_Img[3],character_Info[3], name, 54634, "핑크핑쿠"));
         name = "지니 드래곤";
-        characterChoice.Add(4, new Character(character_Img[4], name, 987654, "퍼랭이"));
+        characterChoice.Add(4, new Character(character_Img[4],character_Info[4], name, 987654, "퍼랭이"));
     }
 
     private void Update()
     {
+        print($"1:{Save_PlayerChoice.instance.isClear_1}");
+        print($"2:{Save_PlayerChoice.instance.isClear_2}");
+        print($"3:{Save_PlayerChoice.instance.isClear_3}");
+        print($"4:{Save_PlayerChoice.instance.isClear_4}");
+
+        #region 스테이지 클리어시 이미지 변경
         if (Save_PlayerChoice.instance.isClear_1 == true)
         {
             dragon2_image.sprite = drangons[1];
             crown_Image[0].sprite = crowns[0];
-            debug_stageClear.text = "Stage 1 클리어";
             Save_PlayerChoice.instance.save_StageClear = 1;
+
+            if(selectCharCount > 1)
+            {
+                print("거부");
+                gameStart.interactable = false;
+            }
+            else
+            {
+                print("승인");
+                gameStart.interactable = true;
+            }
         }
 
-        if (Save_PlayerChoice.instance.isClear_2 == true)
+         if (Save_PlayerChoice.instance.isClear_2 == true)
         {
             dragon3_image.sprite = drangons[2];
             crown_Image[1].sprite = crowns[1];
-            debug_stageClear.text = "Stage 2 클리어";
             Save_PlayerChoice.instance.save_StageClear = 2;
+
+            if (selectCharCount > 2)
+            {
+                gameStart.interactable = false;
+            }
+            else
+            {
+                gameStart.interactable = true;
+            }
         }
 
-        if (Save_PlayerChoice.instance.isClear_3 == true)
+         if(Save_PlayerChoice.instance.isClear_3 == true)
         {
             dragon4_image.sprite = drangons[3];
             crown_Image[2].sprite = crowns[2];
-            debug_stageClear.text = "Stage 3 클리어";
             Save_PlayerChoice.instance.save_StageClear = 3;
+
+            if (selectCharCount > 3)
+            {
+                gameStart.interactable = false;
+            }
+            else
+            {
+                gameStart.interactable = true;
+            }
         }
 
-        if (Save_PlayerChoice.instance.isClear_4 == true)
+         if(Save_PlayerChoice.instance.isClear_4 == true)
         {
             dragon5_image.sprite = drangons[4];
             crown_Image[3].sprite = crowns[3];
-            debug_stageClear.text = "Stage 4 클리어";
             Save_PlayerChoice.instance.save_StageClear = 4;
+
         }
+        if(Save_PlayerChoice.instance.isClear_1 == false)
+        {
+            if (selectCharCount > 0)
+            {
+                gameStart.interactable = false;
+            }
+            else
+            {
+                gameStart.interactable = true;
+            }
+        }
+        #endregion
     }
 
     public void LeftBtn_Clik()
@@ -112,53 +150,51 @@ public class Character_Choice_Manager : MonoBehaviour
         selectCharCount--;
         if (selectCharCount < 0)
         {
-            selectCharCount += 4;
+            selectCharCount += 5;
         }
-        for (int i = 0; i < 4; i++)
+        for (int i = 0; i < 5; i++)
         {
             //character_Img[i].SetActive(false);
             if (characterChoice.ContainsKey(i))
             {
                 Character charf = characterChoice[i];
                 charf.char_Img.SetActive(false);
-                
+                charf.char_Info.SetActive(false);
             }
         }
         //character_Img[selectCharCount].SetActive(true);
         Character charf1 = characterChoice[selectCharCount];
         charf1.char_Img.SetActive(true);
-        gameStart.interactable = false;
+        charf1.char_Info.SetActive(true);
         
     }
 
     public void RightBtn_Clik()
     {
         selectCharCount++;
-        selectCharCount %= 4;
+        selectCharCount %= 5;
 
-        for (int i = 0; i < 4; i++)
+        for (int i = 0; i < 5; i++)
         {
             //character_Img[i].SetActive(false);
             if (characterChoice.ContainsKey(i))
             {
                 Character charf = characterChoice[i];
                 charf.char_Img.SetActive(false);
+                charf.char_Info.SetActive(false);
             }
         }
         //character_Img[selectCharCount].SetActive(true);
         Character charf1 = characterChoice[selectCharCount];
         charf1.char_Img.SetActive(true);
-        gameStart.interactable = false;
+        charf1.char_Info.SetActive(true);
     }
 
-    public void ChoiceBtn_Clik()
+    public void GameStart_Clik()
     {
         if (characterChoice.ContainsKey(selectCharCount))
         {
             Character choiceNow = characterChoice[selectCharCount];
-            debug_Name.text = $"디버그 이름 : {choiceNow.name}";
-            debug_Nft_Number.text = $"디버그 NFT 번호 : {choiceNow.nft_Number}";
-            debug_concept.text = $"디버그 배경설명 : {choiceNow.concept_Txt}";
 
             // 플레이어가 선택한 캐릭터를 저장하기
             Save_PlayerChoice.instance.save_char_Img = choiceNow.char_Img;
@@ -168,32 +204,17 @@ public class Character_Choice_Manager : MonoBehaviour
             gameStart.interactable = true;
             Save_PlayerChoice.instance.char_Prefeb_Choice = Save_PlayerChoice.instance.char_Prefeb[selectCharCount];
         }
-    }
-
-    public void GameStart_Clik()
-    {
         SceneManager.LoadScene(2);
     }
 
-    public void Debug_StageClear_Clik()
+    public void Debug_StageClear()
     {
-        debug_num++;
+        debug_StageClear++;
 
-        if (debug_num == 1)
-        {
-            Save_PlayerChoice.instance.isClear_1 = true;
-        }
-        else if (debug_num == 2)
-        {
-            Save_PlayerChoice.instance.isClear_2 = true;
-        }
-        else if (debug_num == 3)
-        {
-            Save_PlayerChoice.instance.isClear_3 = true;
-        }
-        else if (debug_num == 4)
-        {
-            Save_PlayerChoice.instance.isClear_4 = true;
-        }
+        if(debug_StageClear == 1)       Save_PlayerChoice.instance.isClear_1 = true;
+        else if(debug_StageClear == 2)  Save_PlayerChoice.instance.isClear_2 = true;
+        else if(debug_StageClear == 3)  Save_PlayerChoice.instance.isClear_3 = true;
+        else if(debug_StageClear == 4)  Save_PlayerChoice.instance.isClear_4 = true;
+
     }
 }
